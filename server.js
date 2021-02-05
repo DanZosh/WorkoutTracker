b6b916a6031e9f0536e95398cd6b4c61b1a5dc83
Danny Maier <dannymaier@gmail.com>
2021-02-04 21:23:33 -0800

    i got my html routes to work

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
+// *** Dependencies
+// =============================================================
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+// Sets up the Express App
+// =============================================================
+const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Requiring our models for syncing
 // const db = require("./models")
-
-const app = express();
 
 app.use(logger("dev"));
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+// Static directory
 app.use(express.static('public'));
+
+// Routes
+// =============================================================
+require("./routes/html.js")(app);
+// require("./routes/api.js")(app);
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
