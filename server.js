aa6452f185365ca89d7d0fe4db870246481008bc
Danny Maier <dannymaier@gmail.com>
2021-02-04 20:24:19 -0800

    I got the server working

--- server.js
+++ server.js
@@ -4,4 +4,26 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8000;
 
-const db = require("./models")
+// const db = require("./models")
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+})
+
+
+
+
+
+
+
